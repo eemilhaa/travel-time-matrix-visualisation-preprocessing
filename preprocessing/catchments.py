f0ccdef292fe43baa5332da81a625b9ce6a24ff4
eemilhaa <eemil.haapanen@gmail.com>
2023-06-12 11:31:02 +0300

    try processing entire matrix

--- preprocessing/catchments.py
+++ preprocessing/catchments.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 
 import geopandas as gpd
@@ -13,40 +12,33 @@ GRID = gpd.read_file("./data/grid/")[["YKR_ID", "geometry"]]
 GRID = GRID.to_crs(epsg=4326)
 
 def main():
-    for name in get_txt_files_in_directory(Path("./data/flat_matrix/")):
-        print(name)
+    for path in Path("./data/flat_matrix/").glob("*.txt"):
+        try:
+            traveltimes = pd.read_csv(
+                path, sep=';', na_values=['-1']
+            )
+        except UnicodeDecodeError:
+            continue
+        ykr_id = str(path)[-11:-4]
+        print(f"YKR_ID: {ykr_id}")
         # read
         for mode in config.TRAVEL_MODES:
-            # process modes to gjson
-            pass
+            intrest_times = traveltimes[["from_id", mode]]
 
+            tt_grid = GRID.merge(intrest_times, on=traveltimes["from_id"])
+            tt_grid = tt_grid[["geometry", mode]]
 
-def get_txt_files_in_directory(directory: Path):
-    """A function to get all .txt files from a firectory"""
-    return [name for name in os.listdir(directory) if name.endswith(".txt")]
+            tt_grid['t'] = np.NaN
+            for time in reversed(config.TIMES):
+                mask = (tt_grid[mode] <= time)
+                tt_grid.loc[mask, "t"] = time
 
+            clean = tt_grid.dropna()
+            catchments = clean.dissolve("t").reset_index()
+            catchments = catchments[["geometry", "t"]]
+            catchments.to_file(f"./data/catchments/{2018}_{mode}_{ykr_id}.geojson", driver="GeoJSON")
+            print(f"processed {mode}")
 
-
-# for mode in config.TRAVEL_MODES:
-
-# traveltimes = pd.read_csv(
-#     f"./data/flat_matrix/travel_times_to_ {id}.txt", sep=';', na_values=['-1']
-# )
-# intrest_times = traveltimes[["from_id", interest_col]]
-
-# tt_grid = GRID.merge(intrest_times, on=traveltimes["from_id"])
-# tt_grid = tt_grid[["geometry", interest_col]]
-
-
-# tt_grid['t'] = np.NaN
-
-# for time in reversed(times):
-#     mask = (tt_grid[interest_col] <= time)
-#     tt_grid.loc[mask, "t"] = time
-#     clean = tt_grid.dropna()
-
-# catchments = clean.dissolve("t").reset_index()
-# catchments = catchments[["geometry", "t"]]
 
 if __name__ == "__main__":
     main()
--- preprocessing/config.py
+++ preprocessing/config.py
@@ -1,11 +1,11 @@
 TRAVEL_MODES = [
-    'walk_t',
+    # 'walk_t',
     'bike_s_t',
-    'bike_f_t',
+    # 'bike_f_t',
     'pt_r_t',
-    'pt_m_t',
+    # 'pt_m_t',
     'car_r_t',
-    'car_m_t',
+    # 'car_m_t',
 ]
 
 TIMES = [15, 30, 45, 60, 75]
