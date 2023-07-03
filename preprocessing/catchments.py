f1900788a0eb3a039df543f98c997f654b122949
eemilhaa <eemil.haapanen@gmail.com>
2023-07-03 13:58:04 +0300

    use rglob to skip flattening matrices, refactor into smaller functions

--- preprocessing/catchments.py
+++ preprocessing/catchments.py
@@ -1,3 +1,4 @@
+import argparse
 from pathlib import Path
 
 import geopandas as gpd
@@ -12,7 +13,8 @@ GRID = GRID.to_crs(epsg=4326)
 
 
 def main():
-    for path in Path("./data/flat_matrix/").glob("*.txt"):
+    read_path, write_path = get_args()
+    for path in read_path.rglob("*.txt"):
         try:
             traveltimes = pd.read_csv(
                 path, sep=";", na_values=["-1"]
@@ -21,26 +23,44 @@ def main():
             continue
         ykr_id = str(path)[-11:-4]
         print(f"YKR_ID: {ykr_id}")
-        # read
         for mode in config.TRAVEL_MODES:
-            intrest_times = traveltimes[["from_id", mode]]
-
-            tt_grid = GRID.merge(intrest_times, on=traveltimes["from_id"])
-            tt_grid = tt_grid[["geometry", mode]]
-
-            tt_grid["t"] = np.NaN
-            for time in reversed(config.TIMES):
-                mask = (tt_grid[mode] <= time)
-                tt_grid.loc[mask, "t"] = time
-
-            clean = tt_grid.dropna()
-            catchments = clean.dissolve("t").reset_index()
-            catchments = catchments[["geometry", "t"]]
+            tt_grid = get_grid_with_traveltimes(GRID, traveltimes, mode)
+            catchments = dissolve_grid_to_catchments(
+                tt_grid, breakpoints=config.TIMES, mode=mode
+            )
             catchments.to_file(
-                Path(f"./data/catchments/{2018}_{mode}_{ykr_id}.geojson"),
+                Path(write_path / f"{2018}_{mode}_{ykr_id}.geojson"),
                 driver="GeoJSON"
             )
             print(f"processed {mode}")
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-r", "--read_path", type=str)
+    parser.add_argument("-w", "--write_path", type=str)
+    args = parser.parse_args()
+    read_path = args.read_path
+    write_path = args.write_path
+    return Path(read_path), Path(write_path)
+
+
+def get_grid_with_traveltimes(grid, traveltimes, mode):
+    intrest_times = traveltimes[["from_id", mode]]
+    tt_grid = grid.merge(intrest_times, on=intrest_times["from_id"])
+    tt_grid = tt_grid[["geometry", mode]]
+    tt_grid["t"] = np.NaN
+    return tt_grid
+
+
+def dissolve_grid_to_catchments(tt_grid, breakpoints, mode):
+    for time in reversed(breakpoints):
+        mask = (tt_grid[mode] <= time)
+        tt_grid.loc[mask, "t"] = time
+    clean = tt_grid.dropna()
+    catchments = clean.dissolve("t").reset_index()
+    catchments = catchments[["geometry", "t"]]
+    return catchments
 
 
 if __name__ == "__main__":
