2762cdf92faf9d923c8ad8935a6f05fbc4108520
eemilhaa <eemil.haapanen@gmail.com>
2023-08-11 11:50:28 +0300

    round grid coordinates

--- preprocessing/catchments.py
+++ preprocessing/catchments.py
@@ -4,27 +4,28 @@ from pathlib import Path
 import geopandas as gpd
 import pandas as pd
 import numpy as np
+from shapely import wkt
 
 import config
 
 
 GRID = gpd.read_file(Path("./data/grid/"))[["YKR_ID", "geometry"]]
-GRID = GRID.to_crs(epsg=4326)
 
 
 def main():
-    read_path, write_path = get_args()
-    for path in read_path.rglob("*.txt"):
+    grid = preprocess_grid(GRID)
+    read_path, write_path = get_paths_from_args()
+    for traveltime_filepath in read_path.rglob("*.txt"):
         try:
             traveltimes = pd.read_csv(
-                path, sep=";", na_values=["-1"]
+                traveltime_filepath, sep=";", na_values=["-1"]
             )
         except UnicodeDecodeError:
             continue
-        ykr_id = str(path)[-11:-4]
+        ykr_id = str(traveltime_filepath)[-11:-4]
         print(f"YKR_ID: {ykr_id}")
         for mode in config.TRAVEL_MODES:
-            tt_grid = get_grid_with_traveltimes(GRID, traveltimes, mode)
+            tt_grid = get_grid_with_traveltimes(grid, traveltimes, mode)
             catchments = dissolve_grid_to_catchments(
                 tt_grid, breakpoints=config.TIMES, mode=mode
             )
@@ -35,7 +36,7 @@ def main():
             print(f"processed {mode}")
 
 
-def get_args():
+def get_paths_from_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-r", "--read_path", type=str)
     parser.add_argument("-w", "--write_path", type=str)
@@ -43,6 +44,16 @@ def get_args():
     read_path = args.read_path
     write_path = args.write_path
     return Path(read_path), Path(write_path)
+
+
+def preprocess_grid(grid):
+    def round_coords(feature):
+        rounded_wkt = wkt.dumps(feature, rounding_precision=5)
+        return wkt.loads(rounded_wkt)
+    processed = grid.copy()
+    processed = processed.to_crs(epsg=4326)
+    processed["geometry"] = processed["geometry"].apply(round_coords)
+    return processed
 
 
 def get_grid_with_traveltimes(grid, traveltimes, mode):
